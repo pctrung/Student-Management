06498b5e8a20c55837b0d2c9429a37619d6bf40a
hdxuan <75517919+hdxuan@users.noreply.github.com>
2020-12-05 14:24:51 +0700

    update index.php

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 
-    $connection = new mysqli('localhost', 'root', 'mysql', 'qldiem');
+	$dbhost = "localhost";
+	$dbuser = "root";
+	$dbpass = "mysql";
+	$databaseUse = "qldiem";
+
+    $connection = new mysqli($dbhost, $dbuser, $dbpass, $databaseUse);
 
     if($connection->connect_error){
         die("Kết nối thất bại: " . $connection->connect_error);
@@ -12,20 +17,15 @@
     $sql = "select * from khoa";
 
     $result = $connection->query($sql);
-    
-    $row = $result->fetch_object(); 
-    echo "Ma khoa: " . $row->maKhoa . " - Ten khoa: " . $row->tenKhoa . "<br/>";
-    $row = $result->fetch_object(); 
-    echo "Ma khoa: " . $row->maKhoa . " - Ten khoa: " . $row->tenKhoa . "<br/>";
 
-    // if($result->num_rows > 0){
-    //     while($row = $result->fetch_object()){
-    //         echo "Ma khoa: " . $row->maKhoa . " - Ten khoa: " . $row->tenKhoa . "<br/>";
-    //     }
-    // }
-    // else {
-    //     echo "Khong co du lieu nao";
-    // }
+    if($result->num_rows > 0){
+        while($row = $result->fetch_object()){
+            echo "Ma khoa: " . $row->maKhoa . " - Ten khoa: " . $row->tenKhoa . "<br/>";
+        }
+    }
+    else {
+        echo "Khong co du lieu nao";
+    }
 
 
     $connection->close();
