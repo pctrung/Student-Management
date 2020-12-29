ae040e655d3f82b923ac4a399db595fc0c6f42df
TrungXiIT <chitrung110268@gmail.com>
2020-12-29 17:38:48 +0700

    fix login bug

--- mvc/Controllers/LoginController.php
+++ mvc/Controllers/LoginController.php
@@ -1,37 +1,38 @@
-<?php 
-    class LoginController extends Controller{
-        private $user;
+<?php
+class LoginController extends Controller
+{
+    private $user;
 
-        function __construct(){
-            $this->user = $this->Model("User");
-        }
-        function index(){
-            $this->View("login/index", []);
-        }
-        function login(){
-            if(isset($_POST['btn_login'])){
-                $username = $_POST['username'];
-                $password = $_POST['password'];
-
-                $username = strip_tags($username);
-                $username = addslashes($username);
-                $password = strip_tags($password);
-                $password = addslashes($password);
-
-                $results = $this->user->login($username, $password);
-
-                echo "<script type='text/javascript'>alert('". $results['messages']. "');</script>";
-
-                if($results['isSuccess'] == false){
-                    $this->View("login/index", []);
-                }
-                else if($results['isSuccess'] == true){
-                    $_SESSION['username'] = $username;
-                    header("Location: /Home");
-                }
-            }
-            $this->View("login/index", []);
-        }
+    function __construct()
+    {
+        $this->user = $this->Model("User");
     }
+    function index()
+    {
+        $this->View("login/index", []);
+    }
+    function login()
+    {
+        if (isset($_POST['btn_login'])) {
+            $username = $_POST['username'];
+            $password = $_POST['password'];
 
-?>
+            $username = strip_tags($username);
+            $username = addslashes($username);
+            $password = strip_tags($password);
+            $password = addslashes($password);
+
+            $results = $this->user->login($username, $password);
+
+            echo "<script type='text/javascript'>alert('" . $results['messages'] . "');</script>";
+
+            if ($results['isSuccess'] == false) {
+                $this->View("login/index", []);
+            } else if ($results['isSuccess'] == true) {
+                $_SESSION['username'] = $username;
+                echo "<script>window.location.replace('https://trungpham.me/');</script>";
+            }
+        }
+        $this->View("login/index", []);
+    }
+}
